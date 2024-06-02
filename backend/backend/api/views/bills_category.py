277530dbfa496fc9c447b4154dee60f03a81ce9a
vonot16 <rafaelfeliperm@gmail.com>
2024-06-02 19:26:20 -0300

    Category Create View

--- backend/backend/api/views/bills_category.py
+++ backend/backend/api/views/bills_category.py
@@ -25,3 +25,4 @@ def create(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
